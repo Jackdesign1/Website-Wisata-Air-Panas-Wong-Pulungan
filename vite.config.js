2d332a0494d5a69d193f88c3132335224a9b916b
ferdisaputro <ferdisaputro1@gmail.com>
2025-03-20 10:28:55 +0700

    add models migration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import laravel from 'laravel-vite-plugin';
+
 
 export default defineConfig({
     plugins: [
+        require('flowbite/plugin'),
+        require('flowbite-typography'),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
